c30c25ac64212eae067c6e20ec7d6fb5ad52e579
Lokeswar Reddy Ayyaluri <lokeswarkmit@gmail.com>
2025-12-02 20:32:18 +0530

    Commit after adding Volvo 9600s Service

--- src/main/java/com/demoproject/App.java
+++ src/main/java/com/demoproject/App.java
@@ -3,6 +3,7 @@ package com.demoproject;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello! Welcome to Bus9 tours and Travels (www.bus9.co.in)");
+        System.out.println("Tadipatri -> <- Hyderabad, Volvo 9600s Service");
     }
 }
 
