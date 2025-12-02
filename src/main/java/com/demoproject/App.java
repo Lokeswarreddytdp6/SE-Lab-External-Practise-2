5d961e1db9710efd2d80d2a5caed607dd904c697
Lokeswar Reddy Ayyaluri <lokeswarkmit@gmail.com>
2025-12-02 20:35:34 +0530

    Commit after introducing the Bellary Nellore Service

--- src/main/java/com/demoproject/App.java
+++ src/main/java/com/demoproject/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello! Welcome to Bus9 tours and Travels (www.bus9.co.in)");
         System.out.println("Tadipatri -> <- Hyderabad, Volvo 9600s Service");
+        System.out.println("New Service : Bellary -> <- Nellore. (Eicher (2+1) AC Sleeper)");
     }
 }
 
