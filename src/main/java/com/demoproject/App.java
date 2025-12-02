c5fc3bf72dbcb875b4fbfc4bce625cc6e9fce477
Lokeswar Reddy Ayyaluri <lokeswarkmit@gmail.com>
2025-12-02 19:43:36 +0530

    First Commit

---
+++ src/main/java/com/demoproject/App.java
@@ -0,0 +1,8 @@
+package com.demoproject;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello! Welcome to Bus9 tours and Travels (www.bus9.co.in)");
+    }
+}
+
